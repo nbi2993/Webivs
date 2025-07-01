e8b14144cc8044577d5bf466795a0ed3a842111c
NGUYỄN MINH TRIẾT <167825527+ivsjsc@users.noreply.github.com>
2025-07-01 14:52:54 +0700

    commit github push and sync

--- public/js/firebase-init.js
+++ public/js/firebase-init.js
@@ -1,4 +1,12 @@
-// public/js/firebase-init.js
+import { initializeAppCheck, ReCaptchaV3Provider } from "[https://www.gstatic.com/firebasejs/10.4.0/firebase-app-check.js](https://www.gstatic.com/firebasejs/10.4.0/firebase-app-check.js)";
+
+// ... sau khi initializeApp(firebaseConfig)
+
+const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider('YOUR_RECAPTCHA_V3_SITE_KEY'), // Thay bằng Site Key của bạn
+  isTokenAutoRefreshEnabled: true
+});
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-firestore.js";
